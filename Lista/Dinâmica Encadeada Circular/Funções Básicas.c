1d7a09e078a38a3f2cbe12f517f79e5daf6da06c
Pedro Baschoni <124200114+pedrobaschoni@users.noreply.github.com>
2023-05-31 08:35:24 -0300

    Update Funções Básicas.c

--- Lista/Dinâmica Encadeada Circular/Funções Básicas.c
+++ Lista/Dinâmica Encadeada Circular/Funções Básicas.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 
 typedef struct sLista {
-    char info;
+    int info;
     struct sLista *next;
 }lista;
 
 void inicializar(lista **L) {
-    *L = getnode();
-    (*L)->info = -1;
-    (*L)->next = NULL;
+    *L = NULL;
 }
 
 lista* getnode() {
@@ -26,7 +24,7 @@ int vazia(lista *L) {
     return 0;
 }
 
-void inserir_inicio(lista **L, char elem) {
+void inserir_inicio(lista **L, int elem) {
     lista *q;
     lista *aux = *L;
 
@@ -46,7 +44,72 @@ void inserir_inicio(lista **L, char elem) {
             *L = q;
         }
     } else {
-        printf("\nErro na alocacao do no");
+        printf("\nErro ao alocar o no");
+    }
+}
+
+void inserir_fim(lista **L, int elem) {
+    lista *q;
+    lista *aux = *L;
+
+    q = getnode();
+    if(q != NULL) {
+        q->info = elem;
+
+        if(vazia(*L)) {
+            q->next = q;
+            *L = q;
+        } else {
+            while(aux->next != *L) {
+                aux = aux->next;
+            }
+            aux->next = q;
+            q->next = *L;
+        }
+    } else {
+        printf("\nErro ao alocar o no");
+    }
+}
+
+void remover_inicio(lista **L) {
+    lista *q = *L;
+    lista *aux = *L;
+
+    if(vazia(*L))
+        printf("\nErro! - Lista vazia");
+    else {
+        if((*L)->next == *L) {
+            freenode(q);
+            *L = NULL;
+        } else {
+            while(aux->next != *L) {
+                aux = aux->next;
+            }
+            *L = (*L)->next;
+            aux->next = *L;
+            freenode(q);
+        }
+    }
+}
+
+void remover_fim(lista **L) {
+    lista *q;
+    lista *aux = *L;
+
+    if(vazia(*L))
+        printf("\nErro! - Lista vazia");
+    else {
+        if((*L)->next == *L) {
+            freenode(*L);
+            *L = NULL;
+        } else {
+            while(aux->next->next != *L) {
+                aux = aux->next;
+            }
+            q = aux->next;
+            aux->next = *L;
+            freenode(q);
+        }
     }
 }
 
@@ -58,20 +121,23 @@ void exibir_lista(lista *L) {
         return;
     }
     do {
-        printf("[%c] ", aux->info);
+        printf("[%d] ", aux->info);
         aux = aux->next;
     }while(aux != L);
 }
 
 void main() {
-    lista *L,*no;
-    L->next = no;
+    lista *L;
+
     inicializar(&L);
 
-    inserir_inicio(&L, 'a');
-    inserir_inicio(&L, 'b');
-    inserir_inicio(&L, 'c');
-    inserir_inicio(&L, 'd');
+    inserir_inicio(&L, 1);
+    inserir_inicio(&L, 2);
+    inserir_inicio(&L, 3);
+    inserir_inicio(&L, 4);
+    inserir_fim(&L, 8);
+    remover_inicio(&L);
+    remover_fim(&L);
 
     exibir_lista(L);
 }
